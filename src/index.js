a396b868ad8622d0892172cdc3f1f99df8c95d06
Alena Botyanovskaya <obotyanovska@gmail.com>
2021-08-06 20:17:08 +0300

    add adaptive header + mobile menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import 'modern-normalize';
 import './sass/main.scss';
+// import './js/listeners';
+
+const refs = {
+  mobileMenu: document.querySelector('[data-menu]'),
+  burgerMenu: document.querySelector('.header__burger'),
+};
+
+refs.burgerMenu.addEventListener('click', onBurgerToggle);
+
+function onBurgerToggle() {
+  const expanded =
+    refs.burgerMenu.getAttribute('aria-expanded') === 'true' || false;
+
+  refs.burgerMenu.classList.toggle('is-open');
+  refs.burgerMenu.setAttribute('aria-expanded', !expanded);
+
+  refs.mobileMenu.classList.toggle('is-open');
+}
